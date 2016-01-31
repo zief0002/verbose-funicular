372bacfea0e8c4f14c177ac37abe33f3381d0063
Andrew Zieffler <zief0002@umn.edu>
2016-01-31 16:56:18 -0600

    Updated Assignment 2, added notes and script

---
+++ scripts/02-simple-linear-regression-inference.R
@@ -0,0 +1,64 @@
+##################################################
+### Read in data
+##################################################
+
+math = read.csv(file = "~/Documents/EPsy-8262/data/homework-achievement.csv")
+
+head(math)
+tail(math)
+
+
+
+##################################################
+### Load libraries
+##################################################
+
+library(arm)
+library(ggplot2)
+
+
+
+##################################################
+### Regress achievement on homework
+##################################################
+
+lm.a = lm(achievement ~ homework, data = math)
+
+
+
+##################################################
+### Model output with coefficients-level inferential information
+##################################################
+
+summary(lm.a)
+
+
+
+##################################################
+### ANOVA decomposition and model-level inferential information
+##################################################
+
+anova(lm.a)
+
+
+
+##################################################
+### Plot the fitted model along with the confidence envelope
+##################################################
+
+library(ggplot2)
+
+ggplot(data = math, aes(x = homework, y = achievement)) +
+	geom_smooth(method = "lm", se = TRUE) +
+	xlab("Time Spent on Homework") +
+    ylab("Mathematics Achievement Score") +
+	theme_bw()
+
+
+
+##################################################
+### Watercolor plot
+##################################################
+
+source("~/Documents/EPsy-8251/scripts/S16-scripts/waterColorPlots.R")
+vwReg(achievement ~ homework, data = math, method = lm)
