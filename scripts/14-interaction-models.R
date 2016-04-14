1c53bd4a267267e00e4cd205f1d6892d57a5448a
Andrew Zieffler <zief0002@umn.edu>
2016-04-14 10:48:00 -0500

    Updated interaction script

--- scripts/14-interaction-models.R
+++ scripts/14-interaction-models.R
@@ -2,13 +2,12 @@
 ### Read in the data
 ###################################################
 
-census = read.csv(file = "/Users/andrewz/Documents/EPsy-8262/data/census-sample.csv")
+beauty = read.csv(file = "/Users/andrewz/Documents/epsy-8251/data/beauty.csv")
 
-head(census)
-tail(census)
+head(beauty)
+tail(beauty)
 
 
-census$minority = ifelse(census$ethnicity == "white", 0, 1)
 
 
 
@@ -21,204 +20,123 @@ library(ggplot2)
 
 
 ###################################################
-### Minority differences in income?
+### Interaction of beauty and tenure?
 ###################################################
 
-ggplot(data = census, aes(x = factor(minority), y = income, fill = factor(minority))) +
-	geom_boxplot() +
-	theme_bw() +
-	scale_fill_brewer(palette = "Set1", name = "", labels = c("Non-Minority", "Minority")) +
-	xlab("Minority Status")
+ggplot(data = beauty, aes(x = btystdave, y = avgeval, color = factor(tenured))) +
+  geom_point(alpha = 0.2) +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_bw() +
+  scale_color_brewer(palette = "Set1", name = "", labels = c("Non-Tenured", "Tenured")) +
+  xlab("Beauty")
+
+# Create product term between beauty and tenure
+beauty$int = beauty$btystdave * beauty$tenured
+
+# Fit main-effects model
+lm.me = lm(avgeval ~ btystdave + tenured, data = beauty)
+
+# Fit interaction model
+lm.int =lm(avgeval ~ btystdave + tenured + int, data = beauty)
+
+# Model comparison using nested F-test
+anova(lm.me, lm.int)
+
+# Can also just examine the summary output from the interaction model
+summary(lm.int)
 
 
-ggplot(data = census, aes(x = factor(minority), y = education, fill = factor(minority))) +
-	geom_boxplot() +
-	theme_bw() +
-	scale_fill_brewer(palette = "Set1", name = "", labels = c("Non-Minority", "Minority")) +
-	xlab("Minority Status")
 
-lm.1 = lm(income ~ minority, data = census)
+###################################################
+### Using the : notation
+###################################################
+
+beauty = read.csv(file = "/Users/andrewz/Documents/epsy-8251/data/beauty.csv")
+head(beauty)
+
+lm.1 = lm(avgeval ~ btystdave + tenured + btystdave:tenured, data = beauty)
 summary(lm.1)
 
 
 
 ###################################################
-### Relationship between income and education
+### Interaction of beauty and tenure after controlling for percentevaluating?
 ###################################################
 
-# Overall
-ggplot(data = census, aes(x = education, y = income)) +
-	geom_point(size = 4) +
-	geom_smooth(method = "lm", se = FALSE) +
-	theme_bw()
-
-lm.education = lm(income ~ education, data = census)
-summary(lm.education)
-
-
-
-
-
-
-
-###################################################
-### Relationship between income and education controlling for minority
-###################################################
-
-# intercepts are different, slopes are the same
-lm.2 = lm(income ~ education + minority, data = census)
+lm.2 = lm(avgeval ~ btystdave + tenured + percentevaluating + btystdave:tenured, data = beauty)
 summary(lm.2)
 
+
+# Plot of the model
 plotData = expand.grid(
-	education = 7:20,
-	minority = c(0, 1)
+  btystdave = seq(from = -1.5, to = 1.9, by = 0.1),
+	tenured = c(0, 1),
+  percentevaluating = 74.43
 	)
 
 # Use the interaction model fitted with the colon (:)
 plotData$yhat = predict(lm.2, newdata = plotData)
 
-plotData$minority = factor(plotData$minority,
+plotData$tenured = factor(plotData$tenured,
 	levels = c(0, 1),
-	labels = c("Non-Minority", "Minority")
+	labels = c("Non-Tenured", "Tenured")
 	)
 
-ggplot(data = plotData, aes(x = education, y = yhat, color = minority)) +
+ggplot(data = plotData, aes(x = btystdave, y = yhat, color = tenured)) +
 	geom_line() +
 	theme_bw() +
 	scale_color_brewer(palette = "Set1", name = "") +
-	xlab("Years of education") +
-	ylab("Predicted annual income (in thousands of dollars)")
+	xlab("Beauty") +
+	ylab("Predicted Course Evaluation Score")
 
 
 
 
 ###################################################
-### Is the relationship between income and education different for minorities?
+### Interaction of beauty and tenure after controlling for percentevaluating and female?
 ###################################################
 
-ggplot(data = census, aes(x = education, y = income, color = factor(minority))) +
-	geom_point(size = 4) +
-	#geom_smooth(method = "lm", se = FALSE) +
-	theme_bw() +
-	scale_color_brewer(palette = "Set1")
-
-
-
-
-
-###################################################
-### Create interaction term
-###################################################
-
-census$educMin = census$education * census$minority
-
-
-
-###################################################
-### Fit model 3: Include both main-effects and the interaction term
-###################################################
-
-# intercepts and slopes are different
-lm.3 = lm(income ~ education + minority + educMin, data = census)
+lm.3 = lm(avgeval ~ btystdave + tenured + percentevaluating + female + btystdave:tenured, data = beauty)
 summary(lm.3)
 
 
-
-###################################################
-### Fit model 4: Alternative method
-###################################################
-
-lm.4 = lm(income ~ education + minority + education:minority, data = census)
-summary(lm.4)
-
-
-
-###################################################
-### Plot model
-###################################################
-
+# Plot of the model
 plotData = expand.grid(
-	education = 7:20,
-	minority = c(0, 1)
-	)
+  btystdave = seq(from = -1.5, to = 1.9, by = 0.1),
+  tenured = c(0, 1),
+  female = c(0, 1),
+  percentevaluating = 74.43
+)
 
 # Use the interaction model fitted with the colon (:)
-plotData$yhat = predict(lm.4, newdata = plotData)
+plotData$yhat = predict(lm.3, newdata = plotData)
 
-plotData$minority = factor(plotData$minority,
-	levels = c(0, 1),
-	labels = c("Non-Minority", "Minority")
-	)
+plotData$tenured = factor(plotData$tenured,
+                          levels = c(0, 1),
+                          labels = c("Non-Tenured", "Tenured")
+  )
 
-ggplot(data = plotData, aes(x = education, y = yhat, color = minority)) +
-	geom_line() +
-	theme_bw() +
-	scale_color_brewer(palette = "Set1", name = "") +
-	xlab("Years of education") +
-	ylab("Predicted annual income (in thousands of dollars)")
+plotData$female = factor(plotData$female,
+                          levels = c(0, 1),
+                          labels = c("Male", "Female")
+  )
 
+ggplot(data = plotData, aes(x = btystdave, y = yhat, group = tenured:female, color = tenured)) +
+  geom_line() +
+  theme_bw() +
+  scale_color_brewer(palette = "Set1", name = "") +
+  xlab("Beauty") +
+  ylab("Predicted Course Evaluation Score") +
+  facet_wrap(~female)
 
-
-###################################################
-### Using three dummy variables to represent ethnicity
-###################################################
-
-# Main-effects model
-lm.5 = lm(income ~ education + black + hispanic, data = census)
-summary(lm.5)
-
-
-
-
-# Interaction model
-lm.6 = lm(income ~ education + black + hispanic + education:black + education:hispanic, 
-	data = census)
-summary(lm.6)
-
-# Interaction model 2
-lm.7 = lm(income ~ education + white + hispanic + education:white + education:hispanic, 
-	data = census)
-summary(lm.7)
-
-
-plotData = expand.grid(
-	black = 0:1,
-	hispanic = 0:1,
-	education = 7:9
-	)
-
-
-
-###################################################
-### Plot model
-###################################################
-
-
-# Fit model using ethnicity factor
-lm.8 = lm(income ~ education + ethnicity + education:ethnicity, data = census)
-summary(lm.8)
-
-# Set up plotting data
-plotData = expand.grid(
-	education = 7:20,
-	ethnicity = levels(census$ethnicity)
-	)
-
-# Use the interaction model fitted with the colon (:)
-plotData$yhat = predict(lm.8, newdata = plotData)
-
-# Plot
-ggplot(data = plotData, aes(x = education, y = yhat, color = ethnicity)) +
-	geom_line() +
-	theme_bw() +
-	scale_color_brewer(palette = "Set1", name = "Ethnicity", labels = c("Black", "Hispanic", "White")) +
-	xlab("Years of education") +
-	ylab("Predicted annual income (in thousands of dollars)") 
-
-
-
-
-
+# Alternative plot
+ggplot(data = plotData, aes(x = btystdave, y = yhat, group = tenured:female, color = tenured, linetype = female)) +
+  geom_line() +
+  theme_bw() +
+  scale_color_brewer(palette = "Set1", name = "") +
+  scale_linetype(name = "") +
+  xlab("Beauty") +
+  ylab("Predicted Course Evaluation Score")
 
 
 
